88f775ed71b86ac7bd6746dbd03ffe987d1c8b80
chhotukumar <chhotu8194@gmil.com>
2020-06-13 15:54:36 +0530

    added

--- src/main/java/com/example/Demo/DemoApplication.java
+++ src/main/java/com/example/Demo/DemoApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+  public static void String_data(String data) {
+	  System.out.println("Hi");
+	  if(data.equals("kanha")) {
+		  System.out.println("Wellcome to kmc");
+	  }
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
